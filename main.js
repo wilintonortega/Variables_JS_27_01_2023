da6ab2a15a8dc88a6e84a654c89a3b8222e72e31
wili1991 <wikki9106@gmail.com>
2023-01-27 21:41:23 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let marca="AMD";
+let Nprocesadores=1;
+let activo=true;
+let perro={nombre:"firulais", npatas:4, ladra:"true"};
+
+console.log("El computador "+marca+" tiene "+Nprocesadores+" procesador "+"\n activo: "+activo);
+
+console.log(perro);
