28a33a1271f6c9eb61f656795c19b9cb245d856a
wili1991 <wikki9106@gmail.com>
2023-01-30 18:59:07 -0500

    variables

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ let marca="AMD";
 let Nprocesadores=1;
 let activo=true;
 let perro={nombre:"firulais", npatas:4, ladra:"true"};
-
+console.groupCollapsed("ANIMALES")
+console.table([perro]);
+console.groupEnd();
+console.log(perro);
 console.log("El computador "+marca+" tiene "+Nprocesadores+" procesador "+"\n activo: "+activo);
 
-console.log(perro);
